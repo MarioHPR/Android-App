f69ea90d876d1391f772483d7f81b11a7b6570f5
Mario Henrique Pereira da Rosa <mariopereira398@gmail.com>
2019-11-29 22:14:30 -0300

    primeiro commit app

---
+++ app/src/main/java/com/example/trabalhotcc/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.trabalhotcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText login;
+    private EditText senha;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.user);
+        senha = findViewById(R.id.senha);
+    }
+
+    public void logar(View v){
+        final String log = login.getText().toString();
+        String key = senha.getText().toString();
+        // Cria uma fila para envio de mensagens por Volley
+        RequestQueue filaEnviadoraDeMensagens = Volley.newRequestQueue(this);
+        //Dica: obter o IP abrir o terminal de comando cmd e escrever ipconfig. Procurar por IPv4
+        String urlServidor = "http://192.168.31.6:3000/login";
+
+        // parâmetros para enviar por POST usando um Map
+                final Map<String, String> parametrosPOST = new HashMap<>();
+        // adiciona-se os parâmetros por chave -> valor
+                parametrosPOST.put("login",String.valueOf(log));
+                parametrosPOST.put("senha",String.valueOf(key));
+
+        // cria a requisição de mensagem e tratamento de resposta
+                StringRequest requisicao = new StringRequest(
+                        Request.Method.POST, // 1 - Método usado para enviar mensagem
+                        urlServidor,         // 2 - Endereço do servidor
+                        new Response.Listener<String>() { // 3 - Objeto para tratar resposta
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("DEBUG_LOGIN","RES: "+response);
+                                // {"id":7,"cpf":"1","nome":"w'","dataNascimento":"2001-01-01T02:00:00.000Z",
+                                //  "usuario":"1","senha":"1","id_contato":null,"id_local":null}
+                                //
+                                try {
+                                    JSONObject object = new JSONObject(response);
+                                    if(object.has("id")){
+                                        int id = object.getInt("id");
+                                        // cria u intent
+                                        Intent i = new Intent(getBaseContext(), PrincipalActivity.class);
+                                        i.putExtra("id",id);
+                                        startActivity(i);
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                // exibe o resultado na tela usando Toast
+                                Toast.makeText(
+                                        getBaseContext(),
+                                        "Resultado da soma: "+response,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() { // 4 - Objeto para tratar erro
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // exibe o erro na tela e nos Logs
+                                Toast.makeText(
+                                        getBaseContext(),
+                                        "Erro: "+error,
+                                        Toast.LENGTH_LONG).show();
+                                // log de erro no LogCat
+                                Log.e("TAG_EXEMPLO",
+                                        "Erro encontrado ao tentar enviar mensagem",error);
+                            }
+                        }){
+                    // sobrescrever método getParams para enviar dados por POST
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        return parametrosPOST;
+                    }
+                };
+
+        // envia a mensagem ao servidor
+                filaEnviadoraDeMensagens.add(requisicao);
+
+    }
+}
